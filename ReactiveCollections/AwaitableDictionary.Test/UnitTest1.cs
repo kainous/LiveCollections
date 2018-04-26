6e6ef6684e81806590f380f8276dfdfdf2d286ca
Nicholas Cooper <kainous@hotmail.com>
2018-04-26 18:53:40 -0500

    Better defined Unit Tests

--- ReactiveCollections/AwaitableDictionary.Test/UnitTest1.cs
+++ ReactiveCollections/AwaitableDictionary.Test/UnitTest1.cs
@@ -6,20 +6,32 @@ namespace AwaitableDictionary.Test {
     [TestClass]
     public class UnitTest1 {
         [TestMethod]
-        public async Task TestMethod1() {
+        public async Task ImmediateDictionaryTest() {
             var dict = new AwaitableDictionary<string, int>();
             dict.AddOrReplace("Hello", "Hello".GetHashCode());
             var s = await dict.GetItem("Hello");
+            Assert.AreEqual(s, "Hello".GetHashCode());
+        }
+
+        private async Task AsynchronousAddition(AwaitableDictionary<string, int> dictionary) {
+            await Task.Delay(1000);
+            dictionary.AddOrReplace("World", "World".GetHashCode());
+            dictionary.AddOrReplace("Bar", "Bar".GetHashCode());
+            await Task.Delay(1000);
+            dictionary.AddOrReplace("Foo", "Foo".GetHashCode());
         }
 
         [TestMethod]
         public async Task DelayedDictionaryTest() {
             var dict = new AwaitableDictionary<string, int>();
-            Task.Run(async () => {
-                await Task.Delay(5000);
-                dict.AddOrReplace("World", "World".GetHashCode());
-            });
-            var s = await dict.GetItem("World");
+            var ignore = AsynchronousAddition(dict);
+            var s1 = dict.GetItem("World");
+            var s2 = dict.GetItem("Foo");
+            var s3 = dict.GetItem("Bar");
+            
+            Assert.AreEqual(await s1, "World".GetHashCode());
+            Assert.AreEqual(await s3, "Bar".GetHashCode());
+            Assert.AreEqual(await s2, "Foo".GetHashCode());
         }
     }
 }
