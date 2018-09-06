bc76741a306ba91ee0485d36fdf1f927c869f461
Nick Cooper <Nick.Cooper@Halliburton.com>
2018-09-06 13:13:18 -0500

    Updating to allow for property values on properties

--- LiveCollections/LiveCollections/Interfaces.fs
+++ LiveCollections/LiveCollections/Interfaces.fs
@@ -1,8 +1,9 @@
 namespace System.Collections.LiveCollections
 
 open System
-open System.Reactive.Subjects
+open System.ComponentModel
 open System.Reactive.Linq
+open System.Reactive.Subjects
 
 type KeyedCollectionNotification<'TKey, 'TItem> =
 | Insertion of key:'TKey * item:'TItem
@@ -20,45 +21,55 @@ type IObservableHost<'T> =
 type IProperty<'T> =
   inherit IObservableHost<'T>
   inherit IDisposable
+  inherit INotifyPropertyChanged
+  
+  abstract Value : 'T with get
 
 type ILiveCollection<'TKey, 'TItem> =
   inherit IObservableHost<SourceUpdate<KeyedCollectionNotification<'TKey, 'TItem>>>
 
 [<AutoOpen>]
 module LiveCollectionModule =
-  let keepChanges (source : IProperty<'T>) =
-    let mutable cache = None
-    let subject = new Subject<'T * 'T>()
-    let onNext next =
-      match cache with
-      | None ->
-        cache <- Some next
+  //let keepChanges (source : IProperty<'T>) =
+  //  let mutable cache = None
+  //  let subject = new Subject<'T * 'T>()
+  //  let event = Event<_,_>()
+  //  let onNext next =
+  //    match cache with
+  //    | None ->
+  //      cache <- Some next
 
-      | Some previous ->
-        subject.OnNext(previous, next)
-        cache <- Some next
+  //    | Some previous ->
+  //      subject.OnNext(previous, next)
+  //      cache <- Some next
 
-    let subscription = source.GetObservable().Subscribe(onNext, fun () -> ())
+  //  let subscription = source.GetObservable().Subscribe(onNext, fun () -> ())
 
-    { new IProperty<'T * 'T> with 
-        member this.GetObservable() =
-          subject :> IObservable<'T * 'T>
-        member this.Dispose() =
-          subscription.Dispose()
-          subject.Dispose() }
+  //  { new IProperty<'T * 'T> with 
+  //      member this.GetObservable() =
+  //        subject :> IObservable<'T * 'T>
+  //      member this.Dispose() =
+  //        subscription.Dispose()
+  //        subject.Dispose()
+  //      member this.Value = cache.Value
+  //      [<CLIEvent>]
+  //      member this.PropertyChanged = event.Publish }
 
   let onChanges (source : IProperty<'T>) =
     let mutable cache = None
     let subject = new Subject<'T>()
+    let event = Event<_,_>()
     let onNext next =
       match cache with
       | None ->
         subject.OnNext next
         cache <- Some next
+        event.Trigger(this, PropertyChangedEventArgs "Value")
 
       | Some previous when previous <> next ->
         subject.OnNext next
         cache <- Some next
+        event.Trigger(this, PropertyChangedEventArgs "Value")
 
       | _ -> 
         ()
@@ -70,7 +81,10 @@ module LiveCollectionModule =
           subject :> IObservable<'T>
         member this.Dispose() =
           subscription.Dispose()
-          subject.Dispose() }
+          subject.Dispose() 
+        member this.Value = cache.Value
+        [<CLIEvent>]
+        member this.PropertyChanged = event.Publish }
 
 
   let map (mapping:'TSource -> 'TResult) (source:ILiveCollection<'TKey, 'TSource>) : ILiveCollection<'TKey, 'TResult> =
